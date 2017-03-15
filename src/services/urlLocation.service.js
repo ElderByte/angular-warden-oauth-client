a2614589ac786aa12dfc549265cd00370fa4cd0d
IsNull <p.buettiker@elderbyte.com>
2017-03-15 17:22:07 +0100

    added query parameter update utility functions

--- src/services/urlLocation.service.js
+++ src/services/urlLocation.service.js
@@ -7,6 +7,52 @@ angular.module('wardenOAuth')
         return {
 
             /**
+            * Updates all query parameters in the given url
+            * with the given queryParams map.
+            *
+            * @param uri The url which should be updated
+            * @param queryParams {map} A key-value map interpreted as query key / value
+            * @returns {string} Returns the new url with all query parameters.
+            */
+            updateQueryString : function (uri, queryParams) {
+                var keys = Object.keys(queryParams);
+                for(var i=0;i<keys.length;i++) {
+                    var key = keys[i];
+                    var value = queryParams[key];
+                    uri = this.setQueryParam(uri, key, value);
+                }
+                return uri;
+            };
+
+            /**
+             * Updates the query-param in the given url with the new value.
+             * If the url contains a hash-bang (#) part, the query parameter is append after it.
+             *
+             * @param key The key
+             * @param value The new value
+             * @param url The url to update
+             * @returns {string} Returns the new url with the given query parameter
+             */
+            setQueryParam : function(url, key, value) {
+
+                var parts = url.split('#');
+                var part = parts[parts.length-1]; // Get the last part
+
+                // TODO if the query param already exists, we should update it. (regex-replace)
+
+                var startDelimiter;
+                if(part.includes('?')){
+                    startDelimiter = '&';
+                }else{
+                    startDelimiter = '?';
+                }
+
+                // Append key=value
+
+                return url + startDelimiter + key + '=' + encodeURIComponent(value);
+            },
+
+            /**
             * Returns a map with all query params
             *
             * var params = UrlLocationService.parseQueryParams();
@@ -20,6 +66,8 @@ angular.module('wardenOAuth')
             /**
             * Deletes the query param with the given key from the browser address bar.
             * (Works only with params after #-hash-bang)
+            *
+            * @param key The query parameter key to delete.
             */
             deleteQueryParam: function (key) {
                 console.log("Attempting to delete query param " + key );
@@ -30,6 +78,9 @@ angular.module('wardenOAuth')
             * Returns an absolute URL with the given state/params
             *
             * This method is more robust than $state.href(s,p,{absolute : true})
+            *
+            * @param state The state name
+            * @param params The state params map
             */
             getAbsoluteStateUrl : function(state, params) {
 
@@ -39,6 +90,8 @@ angular.module('wardenOAuth')
 
               return absUrl + "#" + angularRoute;
             },
+
+            // ------------
 
             /**
             * Returns the current absolute url including the path,
