8fc7f65c0ef6033592714b1a4e06611abfe423d2
IsNull <p.buettiker@elderbyte.com>
2017-03-15 17:31:48 +0100

    incremented version to 0.4.5 & dist

--- dist/angular-warden-oauth-client.js
+++ dist/angular-warden-oauth-client.js
@@ -57,6 +57,25 @@ angular.module('wardenOAuth')
 
             var Auth = {
 
+              /**
+              * Signs the given URL with the current JWT token so it can be used
+              * as a download/remote link.
+              *
+              * @param The url which should be authorized.
+              * @returns {string} The authorized url
+              */
+              authorizeUrl : function(url){
+                if(Principal.isAuthenticated()){
+                    var identity = Principal.getIdentity();
+                    if(identity.access_token){
+                        return UrlLocationService.setQueryParam(url, 'token', identity.access_token);
+                    }else{
+                        throw "Can not authorize URL with JWT token - The current principal identity does not have an access token.";
+                    }
+                }else{
+                  throw "Can not authorize URL with JWT token - Not authenticated yet.";
+                }
+              },
 
               /**
                * Sends the user to the login page.
@@ -498,10 +517,69 @@ angular.module('wardenOAuth')
     .factory('UrlLocationService', ["$location", "$state", function ($location, $state) {
         return {
 
+            /**
+            * Updates all query parameters in the given url
+            * with the given queryParams map.
+            *
+            * @param uri The url which should be updated
+            * @param queryParams {map} A key-value map interpreted as query key / value
+            * @returns {string} Returns the new url with all query parameters.
+            */
+            updateQueryString : function (uri, queryParams) {
+                var keys = Object.keys(queryParams);
+                for(var i=0;i<keys.length;i++) {
+                    var key = keys[i];
+                    var value = queryParams[key];
+                    uri = this.setQueryParam(uri, key, value);
+                }
+                return uri;
+            },
+
+            /**
+             * Updates the query-param in the given url with the new value.
+             * If the url contains a hash-bang (#) part, the query parameter is append after it.
+             *
+             * @param key The key
+             * @param value The new value
+             * @param url The url to update
+             * @returns {string} Returns the new url with the given query parameter
+             */
+            setQueryParam : function(url, key, value) {
+
+                var parts = url.split('#');
+                var part = parts[parts.length-1]; // Get the last part
+
+                // TODO if the query param already exists, we should update it. (regex-replace)
+
+                var startDelimiter;
+                if(part.includes('?')){
+                    startDelimiter = '&';
+                }else{
+                    startDelimiter = '?';
+                }
+
+                // Append key=value
+
+                return url + startDelimiter + key + '=' + encodeURIComponent(value);
+            },
+
+            /**
+            * Returns a map with all query params
+            *
+            * var params = UrlLocationService.parseQueryParams();
+            * var p = params['token'];
+            *
+            */
             parseQueryParams: function () {
                 return $location.search();
             },
 
+            /**
+            * Deletes the query param with the given key from the browser address bar.
+            * (Works only with params after #-hash-bang)
+            *
+            * @param key The query parameter key to delete.
+            */
             deleteQueryParam: function (key) {
                 console.log("Attempting to delete query param " + key );
                 $location.search(key, null);
@@ -511,6 +589,9 @@ angular.module('wardenOAuth')
             * Returns an absolute URL with the given state/params
             *
             * This method is more robust than $state.href(s,p,{absolute : true})
+            *
+            * @param state The state name
+            * @param params The state params map
             */
             getAbsoluteStateUrl : function(state, params) {
 
@@ -520,6 +601,8 @@ angular.module('wardenOAuth')
 
               return absUrl + "#" + angularRoute;
             },
+
+            // ------------
 
             /**
             * Returns the current absolute url including the path,
